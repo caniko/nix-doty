0368bd96f658043c06f830f3e308aa3cce266668
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-20 13:31:56 +0200

    chore: bump harbor pins to new rev

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     crane.url = "github:ipetkov/crane";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
   };
 
   outputs = inputs @ {
